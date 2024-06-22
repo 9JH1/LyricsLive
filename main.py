52f8a6512bd4fe918cee458727560d3873ec8e31
3HY <147371934+9JH1@users.noreply.github.com>
2024-06-23 01:06:17 +1200

    example

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ import win32process
 import flask
 from flask_cors import CORS
 import re
-import os
-os.system("title LyricsLive Public Release, software by git@9JH1")
-
+from urllib.parse import urljoin
+import time 
 def get_window_titles():
     window_data = {}
     def enum_windows_callback(hwnd, _):
@@ -25,12 +24,6 @@ def get_window_titles():
             pass
     win32gui.EnumWindows(enum_windows_callback, None)
     return window_data
-
-
-
-
-
-
 # this will work cross-platform
 def scrape_lyrics_containers(url):
     response = requests.get(url)
@@ -46,12 +39,9 @@ def remove_words_in_brackets(text):
     pattern = r'\[[^\]]+\]'
     cleaned_text = re.sub(pattern, '', text)
     return cleaned_text
-
-
-
 app = flask.Flask(__name__,template_folder="")
 CORS(app)
-
+genius_url = ""
 @app.route("/spotify")
 def render_song_info():
     spotify_song = ""
@@ -62,9 +52,9 @@ def render_song_info():
 @app.route("/")
 def render_website():
     return flask.render_template("index.html")
-
 @app.route("/lyrics")
 def render_lyrics():
+    global genius_url
     spotify_song = ""
     for process_name, windows in get_window_titles().items():
         if process_name == "Spotify.exe":
@@ -82,6 +72,24 @@ def render_lyrics():
     print(genius_url)
     lyrics = scrape_lyrics_containers(genius_url)
     return remove_words_in_brackets(str(lyrics)[1:-1])
+
+@app.route("/album_img")
+def get_resource_urls():
+    response = requests.get(genius_url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        resource_urls = set()
+
+        # Find all <img> tags and extract the 'src' attribute
+        for img_tag in soup.find_all('img'):
+            src = img_tag.get('src')
+            if src:
+                if str(urljoin(genius_url, src)).endswith(".jpg") or str(urljoin(genius_url, src)).endswith(".png"):
+                    return str(urljoin(genius_url, src))
+        return None
+    else:
+        print("Failed to retrieve the webpage. Status code:", response.status_code)
+        return None
 
 if __name__ == "__main__": 
     app.run(debug=True)
