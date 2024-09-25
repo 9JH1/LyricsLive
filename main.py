ab97ee2b59ac98c955aded54a366fbb628fde4fe
3HY <147371934+9JH1@users.noreply.github.com>
2024-09-26 09:08:16 +1200

    Update main.py

--- main.py
+++ main.py
@@ -45,50 +45,6 @@ def return_list_packet():
                     spotify_song = windows[0]['title']
     elif platform.system() == "Linux":
         def get_spot_linux():
-            parser = argparse.ArgumentParser()
-            parser.add_argument(
-                '-t',
-                '--trunclen',
-                type=int,
-                metavar='trunclen'
-            )
-            parser.add_argument(
-                '-f',
-                '--format',
-                type=str,
-                metavar='custom format',
-                dest='custom_format'
-            )
-            parser.add_argument(
-                '-p',
-                '--playpause',
-                type=str,
-                metavar='play-pause indicator',
-                dest='play_pause'
-            )
-            parser.add_argument(
-                '--font',
-                type=str,
-                metavar='the index of the font to use for the main label',
-                dest='font'
-            )
-            parser.add_argument(
-                '--playpause-font',
-                type=str,
-                metavar='the index of the font to use to display the playpause indicator',
-                dest='play_pause_font'
-            )
-            parser.add_argument(
-                '-q',
-                '--quiet',
-                action='store_true',
-                help="if set, don't show any output when the current song is paused",
-                dest='quiet',
-            )
-
-            args = parser.parse_args()
-
-
             def fix_string(string):
                 # corrects encoding for the python version used
                 if sys.version_info.major == 3:
@@ -100,22 +56,6 @@ def return_list_packet():
             # Default parameters
             output = fix_string(u'{artist} - {song}')
             trunclen = 35
-            play_pause = fix_string(u'\u25B6,\u23F8') # first character is play, second is paused
-
-            label_with_font = '%{{T{font}}}{label}%{{T-}}'
-            font = args.font
-            play_pause_font = args.play_pause_font
-
-            quiet = args.quiet
-
-            # parameters can be overwritten by args
-            if args.trunclen is not None:
-                trunclen = args.trunclen
-            if args.custom_format is not None:
-                output = args.custom_format
-            if args.play_pause is not None:
-                play_pause = args.play_pause
-
             try:
                 session_bus = dbus.SessionBus()
                 spotify_bus = session_bus.get_object(
